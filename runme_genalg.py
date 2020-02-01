e9d19c6347e2b65ba193fc301698ae5fc4595aed
Didier DUMET <didier.dumet@gmail.com>
2020-02-01 15:43:55 +0100

    import numpy

--- runme_genalg.py
+++ runme_genalg.py
@@ -1,3 +1,22 @@
-from genalg import ga
+import numpy as np
+from genalg import genalg
 
-ga = ga(rosenbroke_func, dims=3, rparents=0.5, popsize=100, pcrossover=0.5, pmutate=0.5, s_gen=300)
+def rosenbroke_func(x):
+    # we return -rosenbroke_func(x) as Genetic Algorithm is a maximisation
+    # algorithm and we're looking for min(rosenbroke_func)
+
+    # The rosenbroke function has exactly one minimum for
+    # dimension=3 at x = (1,1,1) and exactly two minima for
+    # dimension [4,7]
+    
+    return -sum(100*(np.square((x[1:] - np.square(x[:-1]))) + np.square(x[:-1] - 1)))
+
+ga = genalg(rosenbroke_func,
+    dims=3,
+    rparents=0.5,
+    popsize=100,
+    pcrossover=0.5,
+    pmutate=0.5,
+    s_gen=300)
+
+ga.run(verbose=True)
