1d22c09972e90065f21061542a61123d53854791
didier <didier.dumet@gmail.com>
2020-05-01 11:03:25 +0200

    correct Rosenbrock function implementation

--- runme_genalg.py
+++ runme_genalg.py
@@ -1,6 +1,7 @@
 import numpy as np
 from genalg import genalg
 
+
 def rosenbroke_func(x):
     # we return -rosenbroke_func(x) as Genetic Algorithm is a maximisation
     # algorithm and we're looking for min(rosenbroke_func)
@@ -8,15 +9,18 @@ def rosenbroke_func(x):
     # The rosenbroke function has exactly one minimum for
     # dimension=3 at x = (1,1,1) and exactly two minima for
     # dimension [4,7]
-    
-    return -sum(100*(np.square((x[1:] - np.square(x[:-1]))) + np.square(x[:-1] - 1)))
+
+    r = -sum(100*np.square((x[1:] - np.square(x[:-1]))) + np.square(x[:-1]-1))
+
+    return r
+
 
 ga = genalg(rosenbroke_func,
-    dims=3,
-    rparents=0.5,
-    popsize=100,
-    pcrossover=0.5,
-    pmutate=0.5,
-    s_gen=300)
+            dims=3,
+            rparents=0.5,
+            popsize=100,
+            pcrossover=0.5,
+            pmutate=0.5,
+            s_gen=300)
 
 ga.run(verbose=True)
